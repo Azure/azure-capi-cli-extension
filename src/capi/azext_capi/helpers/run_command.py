7f86a39c7d13aba1b6183ed0d4bf5a53f1cc1089
Matt Boersma <Matt.Boersma@microsoft.com>
2022-12-01 16:46:29 -0700

    Install Calico with Helm on workload clusters

--- src/capi/azext_capi/helpers/run_command.py
+++ src/capi/azext_capi/helpers/run_command.py
@@ -6,6 +6,7 @@
 # pylint: disable=missing-docstring
 
 import subprocess
+import time
 
 from azure.cli.core.azclierror import UnclassifiedUserFault
 
@@ -32,3 +33,18 @@ def try_command_with_spinner(cmd, command, spinner_begin_msg, spinner_end_msg,
             if include_error_stdout:
                 error_msg += f"\n{err.stdout}"
             raise UnclassifiedUserFault(error_msg) from err
+
+
+def retry_shell_command(command, attempts=100, delay=3):
+    """Run a shell command, retrying a number of times with a specified delay if it fails."""
+    output = ""
+    for i in range(attempts):
+        try:
+            output = run_shell_command(command)
+            break
+        except subprocess.CalledProcessError as err:
+            logger.info(err)
+            if i == attempts - 1:
+                raise
+            time.sleep(delay)
+    return output
