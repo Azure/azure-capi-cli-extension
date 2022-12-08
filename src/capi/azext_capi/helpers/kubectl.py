e00ef69a4acab7d86a412ba292549d6bde90e4c6
Matt Boersma <Matt.Boersma@microsoft.com>
2022-12-08 14:12:20 -0700

    Don't echo JSON while waiting for nodes with --debug

--- src/capi/azext_capi/helpers/kubectl.py
+++ src/capi/azext_capi/helpers/kubectl.py
@@ -12,7 +12,6 @@ import tempfile
 import os
 import time
 import re
-import json
 
 from azure.cli.core.azclierror import UnclassifiedUserFault
 from azure.cli.core.azclierror import ResourceNotFoundError
@@ -173,27 +172,26 @@ def wait_for_number_of_nodes(number_of_nodes, kubeconfig=None):
     Waits for nodes of specified cluster to get be ready before proceeding.
     Timeout: 15 minutes
     """
-    error_msg = "Not all cluster nodes are Ready after 10 minutes."
-    command = ["kubectl", "get", "nodes", "-o", "json"]
+    command = ["kubectl", "get", "nodes", "--no-headers"]
     command += add_kubeconfig_to_command(kubeconfig)
     timeout = 60 * 15
+    error_msg = f"Not all cluster nodes are Ready after {timeout//60} minutes."
     start = time.time()
     while time.time() < start + timeout:
         try:
-            kubectl_output = run_shell_command(command)
-            json_output = json.loads(kubectl_output)
-            ready_nodes = 0
-            for item in json_output["items"]:
-                for condition in item["status"]["conditions"]:
-                    if condition["type"] == "Ready" and condition["status"] == "True":
-                        ready_nodes += 1
-            if ready_nodes < number_of_nodes:
-                time.sleep(5)
-                continue
-            return
+            output = run_shell_command(command)
         except subprocess.CalledProcessError as err:
             logger.info(err)
             time.sleep(5)
+        # Example output:
+        #   modest-bagpiper-control-plane-ckt6r   Ready      control-plane   2m31s   v1.25.3
+        #   modest-bagpiper-md-0-gxb47            NotReady   <none>          25s     v1.25.3
+        ready_nodes = sum(1 for line in output.splitlines()
+                          if (cols := line.split()) and len(cols) > 1 and cols[1] == "Ready")
+        if ready_nodes < number_of_nodes:
+            time.sleep(5)
+            continue
+        return
     raise ResourceNotFoundError(error_msg)
 
 
