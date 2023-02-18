31079913b4f14c8b342e4c139e781894fae504ab
Yanbing <yawang@microsoft.com>
2023-02-17 22:07:19 -0800

    split lines

--- src/capi/azext_capi/custom.py
+++ src/capi/azext_capi/custom.py
@@ -155,7 +155,8 @@ def create_management_cluster(cmd, cluster_name=None, resource_group_name=None, 
 
 def find_resource_group_name_of_aks_infrastructure(resource_group_name, cluster_name):
     jmespath_query = "nodeResourceGroup"
-    command = ["az", "aks", "show", "--resource-group", resource_group_name, "--name", cluster_name, "--query", jmespath_query, "--output", "tsv"]
+    command = ["az", "aks", "show", "--resource-group", resource_group_name, "--name", cluster_name, "--query",
+               jmespath_query, "--output", "tsv"]
     output = run_shell_command(command)
     result = output.strip()
     return result
@@ -163,7 +164,8 @@ def find_resource_group_name_of_aks_infrastructure(resource_group_name, cluster_
 
 def find_aks_vnet_name(resource_group_name):
     jmespath_query = "[?starts_with(name, 'aks-vnet-')].name | [0]"
-    command = ["az", "network", "vnet", "list", "--resource-group", resource_group_name, "--query", jmespath_query, "--output", "tsv"]
+    command = ["az", "network", "vnet", "list", "--resource-group", resource_group_name, "--query",
+               jmespath_query, "--output", "tsv"]
     output = run_shell_command(command)
     result = output.strip()
     return result
